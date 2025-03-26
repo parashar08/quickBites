bfd25bf5ee95a1b70269a9c830bb0b49ee44ea37
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-27 02:24:41 +0530

    create asyncHandler to handle error

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,30 +1,29 @@
+import { asyncHandler } from '../utils/asyncHandler.js';
 import { validateSignupData } from '../utils/validation.js';
 import { User } from '../models/user.model.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
-export const signupUser = async (req, res) => {
-  try {
-    validateSignupData(req);
-    const { fullName, email, password, role } = req.body;
+export const signupUser = asyncHandler(async (req, res) => {
+  validateSignupData(req);
+  const { fullName, email, password, role } = req.body;
 
-    const existedUser = await User.findOne({ email });
+  const existedUser = await User.findOne({ email });
 
-    if (existedUser) {
-      throw new Error('user with this email exist.');
-    }
-
-    const user = new User({
-      fullName,
-      email,
-      password,
-      role,
-    });
-
-    const savedUser = user.save();
-
-    return res
-      .status(200)
-      .json({ message: 'user added successfully', data: savedUser });
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
+  if (existedUser) {
+    throw new ApiError(409, 'user with this email already exist!');
   }
-};
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+    role,
+  });
+
+  const savedUser = await user.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, savedUser, 'user created successfully!'));
+});
