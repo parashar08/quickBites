04df4e900b65c4cf9db7b1aa0b63602c849c909e
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-27 23:06:37 +0530

    create multer and cloudinary

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+// file first came to server (store in public/temp) then it reupload to cloudinary.
+
+import { v2 as cloudinary } from 'cloudinary';
+import { log } from 'console';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINAY_CLOUD_NAME,
+  api_key: process.env.CLOUDINAY_API_KEY,
+  api_secret: process.env.CLOUDINAY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return 'file not found';
+    }
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    });
+    // file has been uploaded successfully
+    console.log('file is uploaded on cloudinary');
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
