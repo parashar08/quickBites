56e9b936c61d32f885ee9caab3252f6388628f61
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-27 14:00:29 +0530

    create auth middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { User } from '../models/user.model.js';
+import { ApiError } from '../utils/ApiError.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import jwt from 'jsonwebtoken';
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.token || req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+      throw new ApiError(401, 'unauthorized request');
+    }
+
+    const decodedToken = jwt.verify(token, 'quick@Bites$568');
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid token');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error.message || 'Invalid cookies');
+  }
+});
