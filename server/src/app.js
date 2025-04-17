48f367eab2077f6774e63116afb49fe991eea9ff
priyanshuBS <priyanshuparashar08@gmail.com>
2025-04-17 22:22:59 +0530

    add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 import userRoute from './routes/user.route.js';
 
